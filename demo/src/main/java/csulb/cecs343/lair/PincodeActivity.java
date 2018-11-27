ab37a219bd068d460244f0a94d878eb05ea295f9
Anton <anton.shirokov@student.csulb.edu>
2018-11-27 14:42:05 -0800

    Pincode Activity update

--- demo/src/main/java/csulb/cecs343/lair/PincodeActivity.java
+++ demo/src/main/java/csulb/cecs343/lair/PincodeActivity.java
@@ -4,10 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 //import com.google.android.cameraview.demo.R;
 
-public class PincodeActivity extends AppCompatActivity {
+public class PincodeActivity extends AppCompatActivity implements View.OnClickListener {
     DatabaseHelper myDB;
 
     private Button one_button;
@@ -23,9 +24,11 @@ public class PincodeActivity extends AppCompatActivity {
     private Button new_pincode;
     private Button confirm;
 
-
-    Integer pincode;
-    String combined = "";
+    String pincode;
+    String first_pincode_entry;
+    String second_pincode_entry;
+    // Integer pincode;
+    // String combined = "";
 
 
     @Override
@@ -34,115 +37,105 @@ public class PincodeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pincode);
 
         myDB = new DatabaseHelper(this);
+        pincode = "";
+        first_pincode_entry = "";
+        second_pincode_entry = "";
 
         one_button = (Button)findViewById(R.id.one_button);
-        one_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                combined = Integer.toString(pincode) + "1";
-                pincode = new Integer(Integer.parseInt(combined));
-            }
-        });
-
         two_button = (Button)findViewById(R.id.two_button);
-        two_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                combined = Integer.toString(pincode) + "2";
-                pincode = new Integer(Integer.parseInt(combined));
-
-            }
-        });
-
         three_button = (Button)findViewById(R.id.three_button);
-        three_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                combined = Integer.toString(pincode) + "3";
-                pincode = new Integer(Integer.parseInt(combined));
-
-            }
-        });
-
         four_button = (Button)findViewById(R.id.four_button);
-        four_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                combined = Integer.toString(pincode) + "4";
-                pincode = new Integer(Integer.parseInt(combined));
-
-            }
-        });
-
         five_button = (Button)findViewById(R.id.five_button);
-        five_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                combined = Integer.toString(pincode) + "5";
-                pincode = new Integer(Integer.parseInt(combined));
-
-            }
-        });
         six_button = (Button)findViewById(R.id.six_button);
-        six_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                combined = Integer.toString(pincode) + "6";
-                pincode = new Integer(Integer.parseInt(combined));
-
-            }
-        });
         seven_button = (Button)findViewById(R.id.seven_button);
-        seven_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                combined = Integer.toString(pincode) + "7";
-                pincode = new Integer(Integer.parseInt(combined));
-
-            }
-        });
         eight_button = (Button)findViewById(R.id.eight_button);
-        eight_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                combined = Integer.toString(pincode) + "8";
-                pincode = new Integer(Integer.parseInt(combined));
-
-            }
-        });
         nine_button = (Button)findViewById(R.id.nine_button);
-        nine_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                combined = Integer.toString(pincode) + "9";
-                pincode = new Integer(Integer.parseInt(combined));
-
-            }
-        });
         zero_button = (Button)findViewById(R.id.zero_button);
-        zero_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                combined = Integer.toString(pincode) + "0";
-                pincode = new Integer(Integer.parseInt(combined));
-
-            }
-        });
         new_pincode = (Button)findViewById(R.id.new_pincode_button);
-        new_pincode.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
         confirm = (Button)findViewById(R.id.confirm_button);
-        confirm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
+        one_button.setOnClickListener(this);
+        two_button.setOnClickListener(this);
+        three_button.setOnClickListener(this);
+        four_button.setOnClickListener(this);
+        five_button.setOnClickListener(this);
+        six_button.setOnClickListener(this);
+        seven_button.setOnClickListener(this);
+        eight_button.setOnClickListener(this);
+        nine_button.setOnClickListener(this);
+        zero_button.setOnClickListener(this);
+        new_pincode.setOnClickListener(this);
+        confirm.setOnClickListener(this);
+    }
 
-            }
-        });
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.one_button:
+                pincode = pincode + "1";
+                break;
+            case R.id.two_button:
+                pincode = pincode + "2";
+                break;
+            case R.id.three_button:
+                pincode = pincode + "3";
+                break;
+            case R.id.four_button:
+                pincode = pincode + "4";
+                break;
+            case R.id.five_button:
+                pincode = pincode + "5";
+                break;
+            case R.id.six_button:
+                pincode = pincode + "6";
+                break;
+            case R.id.seven_button:
+                pincode = pincode + "7";
+                break;
+            case R.id.eight_button:
+                pincode = pincode + "8";
+                break;
+            case R.id.nine_button:
+                pincode = pincode + "9";
+                break;
+            case R.id.zero_button:
+                pincode = pincode + "0";
+                break;
+            case R.id.new_pincode_button:
 
+                //first check in pincode in the sqlite is null
+
+                if ((!first_pincode_entry.equals("")) && (second_pincode_entry.equals(""))){
+                    Toast.makeText(this, "Enter new pin code " + pincode, Toast.LENGTH_SHORT).show();
+                    first_pincode_entry = "";
+                }
+
+                break;
+            case R.id.confirm_button:
+
+                if ((first_pincode_entry.equals("")) && (!pincode.equals(""))){
+                    Toast.makeText(this, "Your first entry pin code is: " + pincode, Toast.LENGTH_SHORT).show();
+                    first_pincode_entry = pincode;
+                    pincode = "";
+                }
+
+                if ((second_pincode_entry.equals("")) && (!pincode.equals(""))){
+                    second_pincode_entry = pincode;
+                    pincode = "";
+                    if (!first_pincode_entry.equals(second_pincode_entry)){
+                        Toast.makeText(this, "Pin code doesn't match. Re-do pin code.", Toast.LENGTH_SHORT).show();
+                        first_pincode_entry = "";
+                        second_pincode_entry = "";
+                    }
+                    else{
+                        //add pincode to SQLite database
+                        Toast.makeText(this, "Your pin code is set: " + first_pincode_entry, Toast.LENGTH_SHORT).show();
+                        first_pincode_entry = "";
+                        second_pincode_entry = "";
+                    }
+                }
+
+                break;
+        }
     }
 }
