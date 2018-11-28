6df06a4db87bde34a8f5a55562b1785e0ef5c305
Kratos <repinosa@gmail.com>
2018-11-27 16:07:25 -0800

    Small UI and File View updates

---
+++ demo/src/main/java/csulb/cecs343/lair/FileViewActivity.java
@@ -0,0 +1,13 @@
+package csulb.cecs343.lair;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class FileViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fileview);
+    }
+}
