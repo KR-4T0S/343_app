781cd54238be589dcb0208c6bd453930770f3db1
Kratos <repinosa@gmail.com>
2018-11-30 22:15:06 -0800

    File View works now.

--- demo/src/main/java/csulb/cecs343/lair/FileViewActivity.java
+++ demo/src/main/java/csulb/cecs343/lair/FileViewActivity.java
@@ -4,63 +4,86 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.VideoView;
 
-import java.io.File;
-
 public class FileViewActivity extends AppCompatActivity {
-
+    private static final String TAG = "FileViewActivity";
     public static int VIDEO = 1;
     public static int IMAGE = 0;
     ImageView imgView;
     VideoView vidView;
     TextView textView;
+    MediaController mediaController;
+    private int type;
+    private String fileSource;
+    private String fileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);;
         setContentView(R.layout.activity_fileview);
 
+        Log.v(TAG, "onCreate: began execution");
         // Get Parameters
         Bundle b = getIntent().getExtras();
         int _type = b.getInt("type");
         String _fileSource = b.getString("fileSource");
         String _fileName = b.getString("fileName");
 
+        mediaController = new MediaController(this);
         imgView = (ImageView)findViewById(R.id.fileview_imageview);
         vidView = (VideoView)findViewById(R.id.fileview_videoview);
         textView = (TextView)findViewById(R.id.fileview_text_filename);
+        mediaController.setAnchorView(vidView);
+        vidView.setMediaController(mediaController);
 
+        type = _type;
+        fileSource = _fileSource;
+        fileName = _fileName;
+
+        display(type, fileSource, fileName);
+        Log.v(TAG, "onCreate: executed successfully");
+    }
+
+    public void display(int _type, String _fileSource, String _fileName) {
+        Log.v(TAG, "display: began execution");
         if (_type == VIDEO ) {
-            imgView.setVisibility(View.INVISIBLE);
-            vidView.setVisibility(View.VISIBLE);
-            initializeVideo(_fileSource, _fileName);
+                imgView.setVisibility(View.INVISIBLE);
+                vidView.setVisibility(View.VISIBLE);
+                initializeVideo(_fileSource, _fileName);
+        } else {
+                mediaController.setVisibility(View.INVISIBLE);
+                vidView.setVisibility(View.INVISIBLE);
+                imgView.setVisibility(View.VISIBLE);
+                initializeImage(_fileSource, _fileName);
         }
-        if (_type == IMAGE) {
-            imgView.setVisibility(View.INVISIBLE);
-            vidView.setVisibility(View.VISIBLE);
-            initializeImage(_fileSource, _fileName);
-        }
+        Log.v(TAG, "display: executed successfully");
     }
 
     private void initializeVideo(String _fileSource, String _fileName) {
+        Log.v(TAG, "initializeVideo: began execution");
+        vidView.setZOrderOnTop(true);
         Uri uri = Uri.parse(_fileSource);
         vidView.setVideoURI(uri);
-        textView.setText(_fileName);
+        textView.setText(_fileName); // Change page title on bar to file name
+        vidView.requestFocus();
+        vidView.start();
+        Log.v(TAG, "initializeVideo: executed successfully");
     }
 
+
     private void initializeImage(String _fileSource, String _fileName) {
-        File imgFile = new  File(_fileSource);
-        if(imgFile.exists()){
-            Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
-            ImageView myImage = (ImageView) findViewById(R.id.fileview_imageview);
-            myImage.setImageBitmap(myBitmap);
-        }
-        textView.setText(_fileName);
+        Log.v(TAG, "initializeImage: began execution");
+        imgView.bringToFront();
+        Bitmap myBitmap = BitmapFactory.decodeFile(_fileSource);
+        imgView.setImageBitmap(myBitmap);
+        textView.setText(_fileName); // Change page title on bar to file name
+        Log.v(TAG, "initializeImage: executed successfully");
     }
 }
--- demo/src/main/java/csulb/cecs343/lair/Main2Activity.java
+++ demo/src/main/java/csulb/cecs343/lair/Main2Activity.java
@@ -27,9 +27,8 @@ public class Main2Activity extends AppCompatActivity {
         // init_test_filedb(); // test file db
 
         // Run instances of activities here
-        //nit_app_login(); // this initiates with login (main) page, comment out for testing other activities.
+        init_app_login(); // this initiates with login (main) page, comment out for testing other activities.
         //init_test_fileview();
-        init_app_login();
 
     }
 
@@ -95,12 +94,22 @@ public class Main2Activity extends AppCompatActivity {
     }
 
     private void init_test_fileview() {
-        String fileSource = "/storage/files/6334029+_8dc9b60846f726cf2c09446db693f156.jpg";
-        String fileName = "Test Image";
+        // Test Image
+//        String fileSource = "/data/data/com.google.android.cameraview.demo/files/kHPxj2F.jpg";
+//        String fileName = "Test Image";
+//        Intent fileview = new Intent(Main2Activity.this, FileViewActivity.class);
+//        fileview.putExtra("fileSource", fileSource);
+//        fileview.putExtra("fileName", fileName);
+//        fileview.putExtra("type", FileViewActivity.IMAGE);
+
+        // Test Video
+        String vidSource = "/data/data/com.google.android.cameraview.demo/files/infinite dab.mp4";
+        String vidName = "Infinite Dab";
         Intent fileview = new Intent(Main2Activity.this, FileViewActivity.class);
-        fileview.putExtra("fileSource", fileSource);
-        fileview.putExtra("fileName", fileName);
-        fileview.putExtra("type", FileViewActivity.IMAGE);
+        fileview.putExtra("fileSource", vidSource);
+        fileview.putExtra("fileName", vidName);
+        fileview.putExtra("type", FileViewActivity.VIDEO);
+
         startActivity(fileview);
     }
 }
