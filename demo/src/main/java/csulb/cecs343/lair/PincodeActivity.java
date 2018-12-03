6d58cb0e948f279c16d065cf7a6f7d7c7bec94cb
Kratos <repinosa@gmail.com>
2018-12-03 12:54:24 -0800

    UI Changes to main menu and pincode activity.
    Dynamic PIN text will display on activity.

--- demo/src/main/java/csulb/cecs343/lair/PincodeActivity.java
+++ demo/src/main/java/csulb/cecs343/lair/PincodeActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class PincodeActivity extends AppCompatActivity implements View.OnClickListener {
@@ -24,6 +25,7 @@ public class PincodeActivity extends AppCompatActivity implements View.OnClickLi
     private Button zero_button;
     private Button new_pincode;
     private Button confirm;
+    private TextView pin_text;
 
     String pincode;
     String first_pincode_entry;
@@ -52,6 +54,7 @@ public class PincodeActivity extends AppCompatActivity implements View.OnClickLi
         zero_button = (Button)findViewById(R.id.zero_button);
         new_pincode = (Button)findViewById(R.id.new_pincode_button);
         confirm = (Button)findViewById(R.id.confirm_button);
+        pin_text = (TextView) findViewById(R.id.pincode_text_pin);
 
         one_button.setOnClickListener(this);
         two_button.setOnClickListener(this);
@@ -72,60 +75,72 @@ public class PincodeActivity extends AppCompatActivity implements View.OnClickLi
         switch (v.getId()){
             case R.id.one_button:
                 pincode = pincode + "1";
+                pin_text.setText(pincode);
                 break;
             case R.id.two_button:
                 pincode = pincode + "2";
+                pin_text.setText(pincode);
                 break;
             case R.id.three_button:
                 pincode = pincode + "3";
+                pin_text.setText(pincode);
                 break;
             case R.id.four_button:
                 pincode = pincode + "4";
+                pin_text.setText(pincode);
                 break;
             case R.id.five_button:
                 pincode = pincode + "5";
+                pin_text.setText(pincode);
                 break;
             case R.id.six_button:
                 pincode = pincode + "6";
+                pin_text.setText(pincode);
                 break;
             case R.id.seven_button:
                 pincode = pincode + "7";
+                pin_text.setText(pincode);
                 break;
             case R.id.eight_button:
                 pincode = pincode + "8";
+                pin_text.setText(pincode);
                 break;
             case R.id.nine_button:
                 pincode = pincode + "9";
+                pin_text.setText(pincode);
                 break;
             case R.id.zero_button:
                 pincode = pincode + "0";
+                pin_text.setText(pincode);
                 break;
             case R.id.new_pincode_button:
-
-                if ((!first_pincode_entry.equals("")) && (second_pincode_entry.equals(""))){
-                    Toast.makeText(this, "Enter new pin code " + pincode, Toast.LENGTH_SHORT).show();
-                    first_pincode_entry = "";
-                }
+                Toast.makeText(this, "PIN Cleared, Enter new PIN code", Toast.LENGTH_SHORT).show();
+                confirm.setText("Enter");
+                pincode = "";
+                first_pincode_entry = "";
+                second_pincode_entry = "";
+                pin_text.setText("****");
                 break;
             case R.id.confirm_button:
 
                 if ((first_pincode_entry.equals("")) && (!pincode.equals(""))){
-                    Toast.makeText(this, "Your first entry pin code is: " + pincode, Toast.LENGTH_SHORT).show();
                     first_pincode_entry = pincode;
                     pincode = "";
+                    pin_text.setText("****");
+                    confirm.setText("Confirm PIN");
                 }
 
                 if ((second_pincode_entry.equals("")) && (!pincode.equals(""))){
                     second_pincode_entry = pincode;
                     pincode = "";
                     if (!first_pincode_entry.equals(second_pincode_entry)){
-                        Toast.makeText(this, "Pin code doesn't match. Re-do pin code.", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(this, "Pin code doesn't match. Re-do PIN code.", Toast.LENGTH_SHORT).show();
+                        pin_text.setText("****");
                         first_pincode_entry = "";
                         second_pincode_entry = "";
                     }
                     else{
                         myDB.updatePincode(first_pincode_entry);
-
                         Toast.makeText(this, "Your pin code is set: " + first_pincode_entry, Toast.LENGTH_SHORT).show();
                         first_pincode_entry = "";
                         second_pincode_entry = "";
