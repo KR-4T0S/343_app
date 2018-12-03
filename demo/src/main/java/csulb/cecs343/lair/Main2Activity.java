a8f6a70dbca22cb8da62aa5689131ab420796551
Anton <anton.shirokov@student.csulb.edu>
2018-12-03 09:00:50 -0800

    Main Activity
    reorganized and removed not needed buttons

--- demo/src/main/java/csulb/cecs343/lair/Main2Activity.java
+++ demo/src/main/java/csulb/cecs343/lair/Main2Activity.java
@@ -22,34 +22,15 @@ public class Main2Activity extends AppCompatActivity {
 
         // Run instances of activities here
         init_app_login(); // this initiates with login (main) page, comment out for testing other activities.
-
     }
 
-    // Testing and Debug Tools
-    // Place all your testing here,
     private void init_app_login(){
         setContentView(R.layout.activity_main2);
-
-
-        setContentView(R.layout.activity_main2);
-        mProfileMenu = (Button)findViewById(R.id.profile_menu_button);
-        mProfileMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Does nothing yet, but soon!
-
-                Intent profile = new Intent(Main2Activity.this, ProfileMenuActivity.class);
-                startActivity(profile);
-            }
-        });
-
-
 
         mLoginButton = (Button)findViewById(R.id.login_button);
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Does nothing yet, but soon!
                 Intent login = new Intent(Main2Activity.this, LoginActivity.class);
                 startActivity(login);
             }
@@ -58,48 +39,11 @@ public class Main2Activity extends AppCompatActivity {
         mCreateProfileButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Does nothing yet, but soon!
                 Intent intent = new Intent(Main2Activity.this, CreateProfileActivity.class);
                 startActivity(intent);
             }
         });
-
-        mFileMenuButton = (Button) findViewById(R.id.file_menu_button);
-        mFileMenuButton.setOnClickListener(new View.OnClickListener()
-                                           {
-                                               @Override
-                                               public void onClick(View v)
-                                               {
-                                                   //Toast.makeText(Main2Activity.this,
-                                                   //                R.string.file_menu_toast,
-                                                   //                Toast.LENGTH_SHORT).show();
-                                                   Intent intent = new Intent(Main2Activity.this, FileMenuActivity.class);
-                                                   startActivity(intent);
-                                               }
-                                           }
-        );
     }
-
-//    private void init_test_filedb() {
-//        // START Initiate db //
-//        FileDatabaseHelper db = new FileDatabaseHelper(this);
-//        db.getWritableDatabase(); // THIS IS REQUIRED
-//        // END Initiate db //
-//
-//        db.addFolder("ROOT", "/", "");
-//        db.addFolder("videos", "/root","1");
-//        db.addFolder("pictures", "/root", "1");
-//
-//        db.addFile("test file", "jpg","/root", "1");
-//        db.addFile("Fido at the beach", "mp4", "/root/videos","2");
-//        db.addFile("cat playing piano", "mp4", "/root/videos","2");
-//        db.addFile("evolution of dance", "mp4", "/root/videos","2");
-//        db.addFile("family reunion 2016", "png", "/root/pictures","3");
-//        db.addFile("totally not nudes", "png", "/root/pictures","3");
-//
-//        db.close();
-//        db.deleteDB(this);
-//    }
 
     @Override
     public void onBackPressed() {}
